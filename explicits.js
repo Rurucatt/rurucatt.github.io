0a7913e77c54a0566f77adecc4213e09299f9952
Rurucatt <189954343+Rurucatt@users.noreply.github.com>
2026-03-18 00:37:53 -0400

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -52,13 +52,13 @@ define(['questAPI'], function(Quest){
 	 API.addQuestionsSet('mmLikert7',{
         inherit: 'basicSelect',
         answers: [
-            {text:'1 - Strongly disagree', value:1},
-            {text:'2 - Disagree', value:2},
-            {text:'3 - Slightly disagree', value:3},
-            {text:'4 - Neither agree nor disagree', value:4},
-            {text:'5 - Slightly agree', value:5},
-            {text:'6 - Agree', value:6},
-            {text:'7 - Strongly agree', value:7}
+            {text:'Strongly disagree', value:1},
+            {text:'Disagree', value:2},
+            {text:'Slightly disagree', value:3},
+            {text:'Neither agree nor disagree', value:4},
+            {text:'Slightly agree', value:5},
+            {text:'Agree', value:6},
+            {text:'Strongly agree', value:7}
         ]
     });
 
@@ -96,91 +96,91 @@ define(['questAPI'], function(Quest){
 	API.addQuestionsSet('mm18',{
         inherit: 'mmLikert7',
         name: 'mm18_work_ethic',
-        stem: 'Asian Americans generally perform better on standardized exams (i.e., SAT) because of their values in academic achievement.'
+        stem: '1. Asian Americans generally perform better on standardized exams (i.e., SAT) because of their values in academic achievement.'
     });
 
     API.addQuestionsSet('mm13',{
         inherit: 'mmLikert7',
         name: 'mm13_harder_workers',
-        stem: 'Asian Americans are less likely to face barriers at work.'
+        stem: '2. Asian Americans are less likely to face barriers at work.'
     });
 
     API.addQuestionsSet('mm16',{
         inherit: 'mmLikert7',
         name: 'mm16_success_despite_racism',
-        stem: 'Asian Americans make more money because they work harder.'
+        stem: '3. Asian Americans make more money because they work harder.'
     });
 
     API.addQuestionsSet('mm17',{
         inherit: 'mmLikert7',
         name: 'mm17_motivated_success',
-        stem: 'Asian Americans are more likely to persist through tough situations.'
+        stem: '4. Asian Americans are more likely to persist through tough situations.'
     });
 
     API.addQuestionsSet('mm29',{
         inherit: 'mmLikert7',
         name: 'mm29_higher_gpa',
-        stem: 'Asian Americans are more likely to be treated as equal to European Americans.'
+        stem: '5. Asian Americans are more likely to be treated as equal to European Americans.'
     });
 
     API.addQuestionsSet('mm9',{
         inherit: 'mmLikert7',
         name: 'mm9_better_grades',
-        stem: 'Asian Americans are more likely to be good at math and science.'
+        stem: '6. Asian Americans are more likely to be good at math and science.'
     });
 
     API.addQuestionsSet('mm3',{
         inherit: 'mmLikert7',
         name: 'mm3_standardized_exams',
-        stem: 'Asian Americans get better grades in school because they study harder.'
+        stem: '7. Asian Americans get better grades in school because they study harder.'
     });
 
     API.addQuestionsSet('mm5',{
         inherit: 'mmLikert7',
         name: 'mm5_more_money',
-        stem: 'Asian Americans are less likely to experience racism in the United States.'
+        stem: '8. Asian Americans are less likely to experience racism in the United States.'
     });
 
     API.addQuestionsSet('mm8',{
         inherit: 'mmLikert7',
         name: 'mm8_math_science',
-        stem: 'Asian Americans are harder workers.'
+        stem: '9. Asian Americans are harder workers.'
     });
 
     API.addQuestionsSet('mm7',{
         inherit: 'mmLikert7',
         name: 'mm7_persist_tough',
-        stem: 'Despite experiences with racism, Asian Americans are more likely to achieve academic and economic success.'
+        stem: '10. Despite experiences with racism, Asian Americans are more likely to achieve academic and economic success.'
     });
 
     API.addQuestionsSet('mm20',{
         inherit: 'mmLikert7',
         name: 'mm20_less_work_barriers',
-        stem: 'Asian Americans are more motivated to be successful.'
+        stem: '11. Asian Americans are more motivated to be successful.'
     });
 
     API.addQuestionsSet('mm32',{
         inherit: 'mmLikert7',
         name: 'mm32_less_prejudice',
-        stem: 'Asian Americans have stronger work ethics.'
+        stem: '12. Asian Americans have stronger work ethics.'
     });
 
     API.addQuestionsSet('mm10',{
         inherit: 'mmLikert7',
         name: 'mm10_less_racism',
-        stem: 'It is easier for Asian Americans to climb the corporate ladder.'
+        stem: '13. It is easier for Asian Americans to climb the corporate ladder.'
     });
 
     API.addQuestionsSet('mm11',{
         inherit: 'mmLikert7',
         name: 'mm11_treated_as_equals',
-        stem: 'Asian Americans generally have higher grade point averages in school because academic success is more important.'
+        stem: '14. Asian Americans generally have higher grade point averages in school because academic success is more important.'
     });
 
     API.addQuestionsSet('mm23',{
         inherit: 'mmLikert7',
         name: 'mm23_easier_ladder',
-        stem: 'Asian Americans are less likely to encounter racial prejudice and discrimination.'
+        stem: '15. Asian Americans are less likely to encounter racial prejudice and discrimination.'
     });
 
 
