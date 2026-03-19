f3fb1a945c6085a0f6c76d0b65af3524c6098d38
Rurucatt <189954343+Rurucatt@users.noreply.github.com>
2026-03-19 01:39:10 -0400

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -102,7 +102,7 @@ define(['questAPI'], function(Quest){
 	API.addQuestionsSet('mm18',{
         inherit: 'mmLikert7',
         name: 'mm18_work_ethic',
-        stem: mmInstructionsHtml + '1. Asian Americans generally perform better on standardized exams (i.e., SAT) because of their values in academic achievement.'
+        stem: '1. Asian Americans generally perform better on standardized exams (i.e., SAT) because of their values in academic achievement.'
     });
 
     API.addQuestionsSet('mm13',{
@@ -189,8 +189,6 @@ define(['questAPI'], function(Quest){
         stem: '15. Asian Americans are less likely to encounter racial prejudice and discrimination.'
     });
 
-
-	
     API.addSequence([
         {
             inherit:'basicPage',
@@ -202,6 +200,7 @@ define(['questAPI'], function(Quest){
         },
         {
             inherit:'basicPage',
+			text: mmInstructionsHtml,
 			questions: [
                 {inherit:'mm18'},
                 {inherit:'mm13'},
